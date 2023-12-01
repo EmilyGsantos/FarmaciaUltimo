31907924e7a150e963485d6482c8ad879ae91e73
EMILY GONCALVES SANTOS <emily.gsantos@senacsp.edu.br>
2023-12-01 15:48:53 -0300

    Segundo commit

---
+++ administrador.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+    <title>Administrador</title>
+</head>
+<body>
+ 
+</body>
+</html>
+
+
+
+<?php
+// Configurações de conexão com o banco de dados
+$servername = "localhost"; // Host do MySQL
+$username = "root"; // Nome de usuário do MySQL
+$dbname = "farmacia"; // Nome do banco de dados
+ 
+// Cria uma conexão com o banco de dados (sem senha)
+$conn = new mysqli($servername, $username, '', $dbname);
+ 
+// Verificar a conexão com o banco de dados
+if ($conn->connect_error) {
+    die("Falha na conexão com o banco de dados: " . $conn->connect_error);
+}
+ 
+// Consulta SQL para contar os cadastros de cada farmácia
+$sql = "SELECT farmacia, COUNT(*) as quantidade FROM cadastros GROUP BY farmacia";
+$result = $conn->query($sql);
+ 
+if ($result->num_rows > 0) {
+    // Exibir a quantidade de cadastros de cada farmácia
+    while ($row = $result->fetch_assoc()) {
+        $farmacia = $row["farmacia"];
+        $quantidade = $row["quantidade"];
+        echo "Farmácia $farmacia: $quantidade cadastros<br>";
+    }
+} else {
+    echo "Nenhum cadastro encontrado.";
+}
+ 
+// Fechar a conexão com o banco de dados
+$conn->close();
+?>
---
+++ farmacia.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"><!--Link Roboto-->
+    <title>Cadastro de Clientes - Farmácias A, B, C, D</title>
+</head>
+<body>
+    <header><nav></nav></header><br>
+    <h1 id="h1-farm">Cadastro de Clientes</h1><br>
+    <form id="cad" action="processa_cadastro.php" method="post"><br><br>
+        <label for="nome">Nome Completo:</label><br>
+        <input type="text" id="nome" name="nome"><br>
+ 
+        <label for="rg">RG:</label><br>
+        <input type="text" id="rg" name="rg"><br>
+ 
+        <label for="telefone">Telefone:</label><br>
+        <input type="text" id="telefone" name="telefone"><br>
+ 
+        <label for="plano_saude">Plano de Saúde:</label><br>
+        Sim <input type="radio" name="plano_saude" value="Sim">
+        Não <input type="radio" name="plano_saude" value="Não"><br>
+ 
+        <label for="qual_plano">Qual Plano?</label><br>
+        <input type="text" id="qual_plano" name="qual_plano"><br>
+ 
+        <label for="farmacia_popular">Uso de Farmácia Popular:</label><br>
+        Sim <input type="radio" name="farmacia_popular" value="Sim">
+        Não <input type="radio" name="farmacia_popular" value="Não"><br>
+ 
+        <label for="qual_produto">Qual Produto?</label><br>
+        <input type="text" id="qual_produto" name="qual_produto"><br>
+ 
+        <label for="farmacia">Escolha a Farmácia:</label><br>
+        A <input type="radio" name="farmacia" value="A">
+        B <input type="radio" name="farmacia" value="B">
+        C <input type="radio" name="farmacia" value="C">
+        D <input type="radio" name="farmacia" value="D"><br>
+ 
+        <input type="submit" value="Cadastrar"><br>
+    </form>
+
+    <div>
+        <br><button class="bnt-index"><a class="a-index" href="index.php">Voltar</a></button>
+    </div>
+</body>
+</html>
